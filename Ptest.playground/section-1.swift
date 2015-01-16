d110b3975b2c86fa284dadfa4f5903795323213d
55011212010 <istudents@iClassrooms-iMac-48.local>
2015-01-16 15:56:11 +0700

    test

---
+++ Ptest.playground/section-1.swift
@@ -0,0 +1,30 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let x = 56
+let y = 23
+var sum = x+y
+
+sum += 1
+let priceIntInferred = -19
+let priceIntExplicit:Int = -19
+
+let priceInferred = 19.99
+let priceExplicit:Double = 19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+
+
+
+
+
